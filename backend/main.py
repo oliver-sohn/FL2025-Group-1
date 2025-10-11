01fbe7cbf1383253818b903ac6a00751acf4e757
Oliver Sohn <121932532+oliver-sohn@users.noreply.github.com>
2025-10-10 19:47:32 -0500

    change eventdraft schema

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,9 @@ load_dotenv()
 
 SESSION_SECRET = os.getenv("SESSION_SECRET")
 
+# Uncomment the below line to reset the database tables
+# Base.metadata.drop_all(bind=engine)
+
 Base.metadata.create_all(bind=engine)
 
 
